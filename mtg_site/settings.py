cbad424cb453fa07561180af4767cf6f300df47b
Jeremy <tempestj@sheridancollege.ca>
2025-08-05 16:57:09 -0400

    updated settings.py for new media directory

--- mtg_site/settings.py
+++ mtg_site/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mtg_blog','static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
